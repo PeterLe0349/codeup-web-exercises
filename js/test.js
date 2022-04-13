0c7a1c265435441c7af4f094c8e36b8dab3893b1
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-13 13:57:16 -0500

    finished javascript with html lecture

---
+++ js/test.js
@@ -0,0 +1,20 @@
+"use strict";
+//help with debugging
+/*
+console.log("This is from an External file");
+
+let testVar = "Can  you see?";
+console.log(testVar);
+console.log( 5+5, "should be 10", 'hi');
+
+//Alert - alert (argument)
+//Confirm - return a boolean value > confirm(question)
+//Prompt - prompt(stringQuestion) - returns a string
+*/
+//alert ("Hello Umbriel");
+
+let name = prompt("What is your name?");
+console.log(name);
+
+let validName = confirm("You entered: "+ name+ ". Is this correct?");
+console.log(validName);
