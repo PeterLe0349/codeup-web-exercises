609dad233056d759b291745be0cde1abf96b52cc
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-26 17:49:56 -0500

    first part of html ex complete

--- js/hello.js
+++ js/hello.js
@@ -2,12 +2,6 @@
 (function(){
     console.log("Hello World from console");
 
-    document.getElementById('personalHelloBtn').addEventListener('click', sayHello);
-
-    function sayHello(){
-        let myName = prompt("What's your name?")
-        document.getElementById('personalHelloData').innerHTML = `Hello ${myName}!`;
-    }
 
 
 })();
