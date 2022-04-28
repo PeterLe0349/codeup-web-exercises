12702b49484b109600e4480001981d304695ed1d
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-28 09:28:42 -0500

    put more styles on html

--- js/warmups.js
+++ js/warmups.js
@@ -1,29 +1,42 @@
 "use strict";
-let price = 4;
-console.log("Yay! I completed the warm-up!");
+(function(){
 
-function fizzBuzz(input){
-    //give it a value if it's empty
-    if(input === undefined){
-        input = 100;
+
+    function alphaString(input){
+        let strArr = input.split('');
+        strArr.sort();
+        let newStr = strArr.join('');
+        return newStr;
     }
 
-    for(let i= 1; i<=input; i++){
-        if(i%3 == 0  && i%5 == 0){
-            console.log("FizzBuzz");
-        }
-        else if(i%3 == 0){
-            console.log("Fizz");
-        } else if(i%5 == 0){
-            console.log("Buzz");
-        }else {
-            console.log(i);
-        }
-    }
-
-}
-//fizzBuzz();
+    console.log(alphaString('zpadsjkfdfioaabbc'));
+})();
 
 
 
-5%3 =
+
+
+// console.log("Yay! I completed the warm-up!");
+//
+// function fizzBuzz(input){
+//     //give it a value if it's empty
+//     if(input === undefined){
+//         input = 100;
+//     }
+//
+//     for(let i= 1; i<=input; i++){
+//         if(i%3 == 0  && i%5 == 0){
+//             console.log("FizzBuzz");
+//         }
+//         else if(i%3 == 0){
+//             console.log("Fizz");
+//         } else if(i%5 == 0){
+//             console.log("Buzz");
+//         }else {
+//             console.log(i);
+//         }
+//     }
+//
+// }
+// //fizzBuzz();
+
