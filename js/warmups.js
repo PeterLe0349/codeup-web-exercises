986364679f08a4b65f65cb8e77cfc1f41eb364c0
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-03 09:28:02 -0500

    warmpups

--- js/warmups.js
+++ js/warmups.js
@@ -1,17 +1,95 @@
 "use strict";
 (function(){
 
-    function sumOfArrays(arry){
-        let sum = 0;
-
-        for(let x of arry){
-            sum += x;
-        }
-
-        return sum;
+    let product1 = {
+        name: 'Hammar',
+        priceInCents: 400,
+        description: 'It is a a hammar.',
+        inventory: 25034
     }
 
-    console.log(sumOfArrays([1,4,3]));
+    let product2 = {
+        name: 'Computer',
+        priceInCents: 40000,
+        description: 'It is a computer.',
+        inventory: 33000
+    }
+
+    let product3 = {
+        name: 'Ruler',
+        priceInCents: 1000,
+        description: 'It is a ruler.',
+        inventory: 2200
+    }
+
+
+
+    function returnProductEssentialDetails(product){
+        return {name:product.name, priceInCents:product.priceInCents}
+    } // returns...
+// console.log(returnProductEssentialDetails(product1));
+// console.log(returnProductEssentialDetails(product2));
+
+function returnAllProductsEssentialDetails(arry){
+    const arry2 = [];
+    for(let ob of arry){
+        arry2.push(returnProductEssentialDetails(ob));
+    }
+    return arry2;
+
+}
+console.log(returnAllProductsEssentialDetails([product1,product2,product3]));
+    /*
+
+        {
+            name: 'Hammar',
+            priceInCents: 400
+        }
+
+    */
+
+
+    returnProductEssentialDetails(product2); // returns...
+
+    /*
+
+        {
+            name: 'Computer',
+            priceInCents: 40000
+        }
+
+    */
+
+
+    returnProductEssentialDetails(product3); // returns...
+
+    /*
+
+        {
+            name: 'Ruler',
+            priceInCents: 1000
+        }
+
+    */
+
+
+
+
+
+
+
+
+    // function sumOfArrays(arry){
+    //     let sum = 0;
+    //
+    //     for(let x of arry){
+    //         sum += x;
+    //     }
+    //
+    //     return sum;
+    // }
+    //
+    // console.log(sumOfArrays([1,4,3]));
 
     //
     // function alphaString(input){
