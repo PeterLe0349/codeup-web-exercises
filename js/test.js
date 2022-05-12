19d2a1e6e338e484edbd5f187d4daca7ced4ea81
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-12 14:35:32 -0500

    hackerrank practice

--- js/test.js
+++ js/test.js
@@ -1,26 +1,55 @@
 "use strict";
 //Immediate Invoke Function - security protect changes and access
 (function() {
+    'use strict';
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+    var count = 0;
+    var max = 10;
+    var interval = 1000; // interval time in milliseconds
+
+    var intervalId = setInterval(function () {
+        if (count >= max) {
+            clearInterval(intervalId);
+            console.log('All done');
+        } else {
+            count++;
+            console.log('Repeating this line ' + count);
+        }
+    }, interval);
+
+
+
+
+
+
+// DONT GO UNDER IIFE
+}());
+
+
+
+
+
 
 
 //help with debugging
-    /*
-    console.log("This is from an External file");
+/*
+console.log("This is from an External file");
 
-    let testVar = "Can  you see?";
-    console.log(testVar);
-    console.log( 5+5, "should be 10", 'hi');
+let testVar = "Can  you see?";
+console.log(testVar);
+console.log( 5+5, "should be 10", 'hi');
 
-    //Alert - alert (argument)
-    //Confirm - return a boolean value > confirm(question)
-    //Prompt - prompt(stringQuestion) - returns a string
-    */
+//Alert - alert (argument)
+//Confirm - return a boolean value > confirm(question)
+//Prompt - prompt(stringQuestion) - returns a string
+*/
 //alert ("Hello Umbriel");
 
-    let name = prompt("What is your name?");
-    console.log(name);
-
-    let validName = confirm("You entered: " + name + ". Is this correct?");
-    console.log(validName);
-
-}())
+// let name = prompt("What is your name?");
+// console.log(name);
+//
+// let validName = confirm("You entered: " + name + ". Is this correct?");
+// console.log(validName);
--- js/warmups.js
+++ js/warmups.js
@@ -1,37 +1,201 @@
 "use strict";
 
-console.log("hello 05 11");
 
-let strCheck = "rstlne";
+//are two kangaroos going to meet based on velocity and starting distance
+// function kangaroo(x1, v1, x2, v2) {
+//     // Write your code here
+//     let aPoint = x1+v1;
+//     let bPoint = x2+v2;
+//     let met = "NO";
+//     let prevDistance = Math.abs(aPoint-bPoint);
+//     let curDistance = Math.abs(aPoint-bPoint);
+//     let increasingDistance = false;
+//     let jumpCount = 0;
+//
+//     if(aPoint == bPoint){
+//         met = "YES";
+//         increasingDistance = true;
+//     }
+//
+//     while(increasingDistance == false){
+//         jumpCount++;
+//         aPoint = x1+ (v1*jumpCount);
+//         bPoint = x2 + (v2*jumpCount);
+//         curDistance = Math.abs(aPoint-bPoint);
+//         if(curDistance > prevDistance){
+//             break;
+//         }
+//         if(curDistance == 0){
+//             met = "YES";
+//             break;
+//         }
+//     }
+//
+//
+//     return met;
+//
+// }
 
-function makeObjectFromString(str, stringCheck){
-    const objStr = {};
 
-    objStr.string = str;
-    objStr.size = str.length;
-    objStr.containsLettersFromRSTLNE = containLetters(str, stringCheck);
-    objStr.isOneWord = !str.includes(" ");
 
-    return objStr;
 
-}
 
-function containLetters(str, stringCheckList){
-    for(let i = 0; i< str.length; i++){
-        for(let j = 0; j < stringCheckList.length; j++){
-            if( str[i] === stringCheckList[j]){
-                console.log(str[i] + " and " + str[j]);
-                return true;
-            }
-      //USE string.contains("string")
-        }
-    }
-    return false;
-}
+// let countP = 0;
+// countP++;
+// console.log(countP);
 
-console.log(makeObjectFromString("tacocat", strCheck));
-console.log(makeObjectFromString("JJ J", strCheck));
-console.log(makeObjectFromString("love", strCheck));
+
+// * Complete the 'countApplesAndOranges' function below.
+// *
+// * The function accepts following parameters:
+//     *  1. INTEGER s
+// *  2. INTEGER t
+// *  3. INTEGER a
+// *  4. INTEGER b
+// *  5. INTEGER_ARRAY apples
+// *  6. INTEGER_ARRAY oranges
+// */
+
+// function countApplesAndOranges(s, t, a, b, apples, oranges) {
+//     // Write your code here
+//     let appCount = 0;
+//     let orCount = 0;
+//     for(let i=0; i < apples.length; i++){
+//         let dropPoint = apples[i] + a;
+//         if ( (dropPoint >= s) && (dropPoint <= t)){
+//             appCount++;
+//         }
+//     }
+//     console.log(orCount);
+//     for(let i=0; i < oranges.length; i++){
+//         let dropPoint = oranges[i] + b;
+//         if ( (dropPoint >= s) && (dropPoint <= t)){
+//             orCount++;
+//         }
+//     }
+//     console.log(orCount);
+//
+//
+// }
+
+
+
+
+// ROUNDING UP at 5 for grades
+// function gradingStudents(grades) {
+//     // Write your code here
+//     for(let i= 0; i < grades.length; i++){
+//         if(grades[i] < 38){
+//             continue;
+//         } else if( grades[i] == 100){
+//             continue;
+//         } else {
+//             let roundedUp = Math.ceil(grades[i]/5) * 5;
+//             if( (roundedUp - grades[i]) < 3   ){
+//                 grades[i] = roundedUp;
+//             }else {
+//                 continue;
+//             }
+//         }
+// return grades;
+//     }
+//
+// }
+
+
+
+
+
+
+// function timeConversion(s) {
+//     // Write your code here
+//     if (s.includes("AM")) {
+//         if (s.substring(0, 2) == "12") {
+//             return "00" + s.substring(2, s.length - 2);
+//         } else {
+//             return s.substring(0, s.length - 2);
+//         }
+//     } else {
+//         if (s.substring(0, 2) == "12") {
+//             return "12" + s.substring(2, s.length - 2);
+//         } else {
+//             let zuluNumber = parseInt(s.substring(0, 2)) + 12;
+//             return (zuluNumber + s.substring(2, s.length - 2));
+//         }
+//
+//     }
+// }
+
+    // hh:mm:ssAM
+// console.log(timeConversion("11:32:45AM"));
+// console.log(timeConversion("11:32:45PM"));
+
+
+// const arry1 = [1234134,12, 52,1123];
+// console.log(arry1);
+// arry1.sort(function(a,b){return a-b});
+// console.log(arry1);
+
+
+// console.log("hello 05 12");
+
+// function staircase(n) {
+//     for (let i = 0; i < n; i++) {
+//         let a = "1".repeat(n-1-i);
+//         let b= "#".repeat(i+1)
+//         console.log(a.concat(b));
+//     }
+// }
+//
+//
+//
+// console.log(staircase(3));
+
+
+
+
+// const arry1 = [];
+// arry1.push(([1,4,5]));
+// console.log(arry1);
+// arry1.push(([2,4,5]));
+// console.log(arry1);
+// arry1.push(([4,5,7]));
+// console.log(arry1);
+//
+
+
+
+
+// let strCheck = "rstlne";
+//
+// function makeObjectFromString(str, stringCheck){
+//     const objStr = {};
+//
+//     objStr.string = str;
+//     objStr.size = str.length;
+//     objStr.containsLettersFromRSTLNE = containLetters(str, stringCheck);
+//     objStr.isOneWord = !str.includes(" ");
+//
+//     return objStr;
+//
+// }
+//
+// function containLetters(str, stringCheckList){
+//     for(let i = 0; i< str.length; i++){
+//         for(let j = 0; j < stringCheckList.length; j++){
+//             if( str[i] === stringCheckList[j]){
+//                 console.log(str[i] + " and " + str[j]);
+//                 return true;
+//             }
+//       //USE string.contains("string")
+//         }
+//     }
+//     return false;
+// }
+//
+// console.log(makeObjectFromString("tacocat", strCheck));
+// console.log(makeObjectFromString("JJ J", strCheck));
+// console.log(makeObjectFromString("love", strCheck));
 
 
 
