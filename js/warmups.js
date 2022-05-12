7c87f22ae61fcf6c4f100b782cf0cff7dc017892
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-12 15:43:05 -0500

    solved two kangaroo jump problem

--- js/warmups.js
+++ js/warmups.js
@@ -1,6 +1,86 @@
 "use strict";
 
 
+
+
+
+
+
+
+
+// SEE IF TWO Kangaroos will meet based on starting point and velocity
+// function kangaroo(x1, v1, x2, v2) {
+//     // Write your code here
+//     let aPoint = x1+v1;
+//     let bPoint = x2+v2;
+//     if( (aPoint == bPoint) || (x1==x2)){
+//         return "YES";
+//     }
+//     if ( (aPoint - bPoint) > 0 ){
+//         if(v2 > v1){
+//             if((aPoint-bPoint)%(v2-v1) == 0){
+//                 return "YES";
+//             }else{
+//                 return "NO";
+//             }
+//         }else {
+//             return "NO";
+//         }
+//     } else {
+//         if(v1 > v2){
+//             if((aPoint-bPoint)%(v1-v2) == 0){
+//                 return "YES";
+//             }else{
+//                 return "NO";
+//             }
+//         }else {
+//             return "NO";
+//         }
+//     }
+//
+// }
+
+
+// function kangaroo(x1, v1, x2, v2) {
+//     // Write your code here
+//     let aPoint = x1+v1;
+//     let bPoint = x2+v2;
+//     let met = "NO";
+//     let prevDistance = Math.abs(aPoint-bPoint);
+//     let curDistance = Math.abs(aPoint-bPoint);
+//     let increasingDistance = false;
+//     let jumpCount = 0;
+//
+//     if(aPoint == bPoint){
+//         met = "YES";
+//         increasingDistance = true;
+//     }
+//
+//     while(increasingDistance == false){
+//         jumpCount++;
+//         aPoint = x1+ (v1*jumpCount);
+//         bPoint = x2 + (v2*jumpCount);
+//         curDistance = Math.abs(aPoint-bPoint);
+//         if(curDistance > prevDistance){
+//             break;
+//         }
+//         if(curDistance == 0){
+//             met = "YES";
+//             break;
+//         }
+//     }
+//
+//
+//     return met;
+//
+// }
+
+
+
+
+
+
+
 //are two kangaroos going to meet based on velocity and starting distance
 // function kangaroo(x1, v1, x2, v2) {
 //     // Write your code here
