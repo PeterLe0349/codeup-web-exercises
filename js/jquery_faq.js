69ceb23caa2f0999fa0cd67241622eb2038d5298
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-18 14:05:56 -0500

    jquery faq and bonus complete

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,7 +1,12 @@
 "use strict";
 $(document).ready(function(){
-   $(this).find('dd').toggleClass('invisible');
-
+   // $(this).find('dd').toggleClass('invisible');
+   $('#showBtn').click(function(){
+      $('dd').toggleClass("invisible");
+   });
+   $('dt').click(function(){
+      $(this).next().toggleClass("invisible");
+   });
 
 
 });
