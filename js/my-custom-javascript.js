3af33c5f0916cdce9d4ef9a7b7ed1b96cf7eb6e1
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-18 08:35:05 -0500

    jquery events ex complete

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -11,8 +11,46 @@ $(document).ready(function() {
     // alert(contents2);
     // $('.codeup').css('border', '1px solid red');
 
-    $('li').css('font-size', '20px');
-    $('h1, p, li').css('background-color','yellow');
-    let contents3 = $('h1').html();
-    alert(contents3);
+    // $('li').css('font-size', '20px');
+    // $('h1, p, li').css('background-color','yellow');
+    // let contents3 = $('h1').html();
+    // alert(contents3);
+    //
+    // $('#marvel').css('color', 'purple');
+
+    // $("#codeup").click(function(){
+    //     alert("h1 with ID codeup was clicked!");
+    // });
+    //
+    // $('#codeup').dblclick(function(e){
+    //     alert("h1 with ID codeup was DOUBLE clicked!");
+    // });
+    //
+    // $('#left-column').hover(
+    //     function(){
+    //         $(this).css('background-color', 'green');
+    // },
+    //     function(){
+    //         $(this).css('background-color', 'red');
+    //     }
+    //     );
+
+    $('h1').click(function(){
+        $('h1').css('background-color', 'red');
+    });
+
+    $('p').dblclick(function(){
+        $('p').css('font-size', '18px');
+    });
+
+    $('li').hover(
+        function(){
+            $('li').css('color', 'red');
+        },
+        function(){
+            $('li').css('color', 'black');
+        }
+    );
+
+
 });
